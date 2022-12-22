42cd037ed0b3c0031b244c8af7fbab21cb07d156
maddy <madhav3195@gmail.com>
2022-12-23 01:56:14 +0530

    secind

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Madhav</p>
+        <p>Hello Madddy</p>
         <a
           className="App-link"
           href="https://reactjs.org"
